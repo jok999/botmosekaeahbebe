ef4109331bf67ebc0c7a4aca74d8b43fdebacb34
jok999 <42048536+jok999@users.noreply.github.com>
2018-10-22 21:21:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ const fs = require('fs');
 
 const Client = new Discord.Client({disableEveryone: true});
 
-const prefix = "&&";
+const prefix = "&";
 /////////////////////////
 ////////////////////////
 
@@ -331,7 +331,7 @@ function play(guild, song) {
 
 
    client.on("message", message => {
- if (message.content === "موسيقى") {
+ if (message.content === "%help") {
   const embed = new Discord.RichEmbed() 
       .setColor("RANDOM")
       .setDescription(`
@@ -360,7 +360,23 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
 
 
 
-
+ client.on('ready', function(){	
+    var ms = 10000 ;	
+    var setGame = ['&help'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+              client.user.setGame(setGame[i],`https://www.twitch.tv/vv`);
+}, ms);	
+	
+});
 
 
 
